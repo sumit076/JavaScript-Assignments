7d8134a011d24f77fced1651bd40bb790f984eb0
Sumit Saha <Sumit360saha@gmail.com>
2020-07-16 15:33:40 +0530

    Initial Commit

---
+++ Day 2/Question 2.js
@@ -0,0 +1,11 @@
+var str = "Please locate where 'locate' occurs!";
+var pos = str.indexOf("locate");
+
+
+var str = "Apple, Banana, Kiwi";
+var res = str.slice(7, 13);
+
+var str = "Apple, Banana, Kiwi";
+var res = str.slice(-12, -6);
+
+
