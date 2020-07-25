552313751cf44d1adf9e5b2fbabd230f10e97411
Sumit Saha <Sumit360saha@gmail.com>
2020-07-25 23:33:25 +0530

    Add files via upload

---
+++ Day 6/Question 1.js
@@ -0,0 +1,9 @@
+console.log(Day_6_Assignment);
+
+let name = "Sumit Saha";
+let skill = "hacking";
+let weakness = "Tokyo";
+
+console.log("The name is " + name + " .His skill is " + skill + " and his weakness is " + weakness);
+
+console.log(`The name is ${name}. His skill is ${skill} and his weakness is ${weakness}.`);
